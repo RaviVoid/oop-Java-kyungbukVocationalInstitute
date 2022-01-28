9a2024f299307661dc5262e9d6bf71139d2af86b
ChoiRavi <RaviVoid@gmail.com>
2022-01-28 10:03:36 +0900

    엘리베이터 실습

--- src/ElevatorExam2.java
+++ src/ElevatorExam2.java
@@ -8,29 +8,37 @@ public class ElevatorExam2 {
 		e.floorMax = 4;
 		e.floorMin = 1;
 		e.weight = 550;
-		e.nopsMax = 8;//8명
-		
+		e.nopsMax = 8;// 8명
+
 		Scanner scan = new Scanner(System.in);
-				
-		System.out.println("현재 계신 층을 눌러주세요");
-		switch (scan.next()) {
-		case "1상":
-			e.currFloor = 1;
+
+		while (true) {
+			System.out.println("현재 계신 층을 눌러주세요");
+			switch (scan.next()) {
+			case "1상":
+				e.currFloor = 1;
+				break;
+			case "2상":
+			case "2하":
+				e.currFloor = 2;
+				break;
+			case "3상":
+			case "3하":
+				e.currFloor = 2;
+				break;
+			case "4하":
+				e.currFloor = 4;
+				break;
+			default:
+				break;
+			}
 			e.openDoor();
 			System.out.println("탑승했습니다.");
 			e.closeDoor();
 			e.displayMenu();
-			break;
-
-		default:
-			break;
+			
 		}
-		
-		
-		
-	}
 
-	
-	
+	}
 
 }
