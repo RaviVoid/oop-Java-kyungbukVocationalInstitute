0022e822128244d630f3e26370cb26c9debecbd7
smart9 <smart9@DESKTOP-OHMEF6D>
2022-02-08 13:35:20 +0900

    상속관계

---
+++ src/ext/Board.java
@@ -0,0 +1,23 @@
+package ext;
+
+public class Board {
+	private String title;//제목
+	private String content;//내용
+	
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+
+}
---
+++ src/ext/Gallery.java
@@ -0,0 +1,16 @@
+package ext;
+
+public class Gallery extends Board {
+
+	private String imgFile;//그림파일
+	
+
+	public String getImgFile() {
+		return imgFile;
+	}
+	public void setImgFile(String imgFile) {
+		this.imgFile = imgFile;
+	}
+	
+	
+}
