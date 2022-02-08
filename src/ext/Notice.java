83b6fcee4ceaf3b637ece08a59ab3269ab044a4e
smart9 <smart9@DESKTOP-OHMEF6D>
2022-02-08 15:23:03 +0900

    학생교수상속

--- src/ext/Notice.java
+++ src/ext/Notice.java
@@ -1,19 +1,6 @@
 package ext;
 
 public class Notice extends Board {
+	private String category;
 
-	@Override
-	public String getFile() {
-		// TODO Auto-generated method stub
-		return super.getFile();
-	}
-
-	@Override
-	public void setFile(String file) {
-		// TODO Auto-generated method stub
-		super.setFile(file);
-	}
-	
-	
-	
 }
